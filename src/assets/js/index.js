4b7af786099e4cd3bb9d7877a7015ea428d80973
Ian Parker <parker.ian.dev@gmail.com>
2024-09-23 14:21:43 +1000

    redploy

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -25,6 +25,7 @@ function Dashboard() {
 
   data.forEach((activity, i) => {
 
+    // loop over data and add tiles one by one
     Object.keys(activity.timeframes).forEach((timeframe) => {
       const tileInfo = {
         area: gridAreas[i],
